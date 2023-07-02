275084835dbcb03c074b6405eedb3684c583dd4a
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-07-02 16:45:41 +0200

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text('Ricky & Morty'))),
+      ),
     );
   }
 }
