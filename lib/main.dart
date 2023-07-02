ac554ab99b4e1346149563c317f76d150f618dbd
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-07-02 18:11:29 +0200

    create FirstPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'first page/first_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,44 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Center(child: Text('Ricky & Morty'))),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 100,
-                width: 300,
-                margin: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'All characters',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 100,
-                width: 300,
-                margin: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Favourite characters',
-                    style: TextStyle(fontSize: 30),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: const FirstPage(),
     );
   }
 }
