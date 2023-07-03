958d06b95bcf1285e916a0ab9f39aac316eeb354
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-07-03 10:15:04 +0200

    Add all characters page file

---
+++ lib/all characters page/all_characters_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllCharactersPage extends StatelessWidget {
+  const AllCharactersPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
